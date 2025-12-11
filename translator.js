b86a0836acde5a2f605c2a309d694392482f606c
Suji-211 <sujijohn211@gmail.com>
2025-12-11 15:22:06 +1100

    adds a function to detect the input

--- translator.js
+++ translator.js
@@ -48,7 +48,25 @@ function translateSentence(sentence){
     return morseWords.join(" / ");
 }
 
+// to find the input if its morse or english 
 
+function detectLanguage(input){
+    const trimmed = input.trim();
+    const morsepattern = /^[.\.-\/\s]+$/;
+    if (trimmed === "") {
+        return "english";
+    }    
+    if (morsepattern.test(trimmed)){
+        return "morse";
+    } 
+    return "english";
+}
+
+function autoTranslate(input){
+    const type = detectLanguage(input);
+    return type === "english" ? translateWord(input): translateSentence(input);
+}
  window.getMorseForLetter = getMorseForLetter;
  window.translateWord = translateWord;
  window.translateSentence = translateSentence;
+ window.autoTranslate = autoTranslate;
